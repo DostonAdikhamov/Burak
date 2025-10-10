e149f7af673e124715821f585a5ce9993cca6fc1
Doston Adikhamov <adikhamov94@gmail.com>
2025-10-10 17:30:44 +0900

    Task G done

--- train.ts
+++ train.ts
@@ -1,5 +1,5 @@
 console.log("Task G")
-function getHighestIndex(arr: number[]) {
+function yuqoriIndex(arr: number[]) {
     let maxIndex = 0;
 
     for (let i = 1; i < arr.length; i++) {
@@ -12,5 +12,5 @@ function getHighestIndex(arr: number[]) {
 }
 
 console.log('================================');
-console.log("ANSWER ONE:", getHighestIndex([5, 21, 12, 25, 8]));
-console.log("ANSWER TWO:", getHighestIndex([5, 31, 17, 21, 0]));
+console.log("ANSWER ONE:", yuqoriIndex([5, 21, 12, 25, 8]));
+console.log("ANSWER TWO:", yuqoriIndex([5, 31, 17, 21, 0]));
