f883893bf575ad20255f15672980bfb28be61311
Doston Adikhamov <adikhamov94@gmail.com>
2025-10-10 17:30:11 +0900

    Task G bajarildi

---
+++ train.ts
@@ -0,0 +1,16 @@
+console.log("Task G")
+function getHighestIndex(arr: number[]) {
+    let maxIndex = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > arr[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+}
+
+console.log('================================');
+console.log("ANSWER ONE:", getHighestIndex([5, 21, 12, 25, 8]));
+console.log("ANSWER TWO:", getHighestIndex([5, 31, 17, 21, 0]));
