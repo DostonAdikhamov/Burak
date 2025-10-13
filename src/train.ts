36232a590315989df6e69911807002e542d882f8
Doston Adikhamov <adikhamov94@gmail.com>
2025-10-13 16:41:18 +0900

    feat: Task H is done

--- src/train.ts
+++ src/train.ts
@@ -1,16 +1,23 @@
-console.log("Task G")
-function yuqoriIndex(arr: number[]) {
-    let maxIndex = 0;
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > arr[maxIndex]) {
-            maxIndex = i;
-        }
-    }
-
-    return maxIndex;
+console.log("TASK H");
+function positiveToString(arr: number[]) {
+  const positiveNumbers: number[] = arr.filter((num: number) => num > 0);
+  return positiveNumbers.join(",");
 }
+console.log(positiveToString([10, -4, 0, 23, -9, 5]));
 
-console.log('================================');
-console.log("ANSWER ONE:", yuqoriIndex([5, 21, 12, 25, 8]));
-console.log("ANSWER TWO:", yuqoriIndex([5, 31, 17, 21, 0]));
+// console.log("Task G")
+// function yuqoriIndex(arr: number[]) {
+//     let maxIndex = 0;
+
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > arr[maxIndex]) {
+//             maxIndex = i;
+//         }
+//     }
+
+//     return maxIndex;
+// }
+
+// console.log('================================');
+// console.log("ANSWER ONE:", yuqoriIndex([5, 21, 12, 25, 8]));
+// console.log("ANSWER TWO:", yuqoriIndex([5, 31, 17, 21, 0]));
