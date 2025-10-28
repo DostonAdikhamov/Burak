a2441286caf15849815fbe37f16d059dee9bff89
Doston Adikhamov <adikhamov94@gmail.com>
2025-10-29 07:51:07 +0900

    feat: createNewProduct business logic by restaurant member

---
+++ src/libs/types/product.ts
@@ -0,0 +1,33 @@
+import { ObjectId } from "mongoose";
+import {
+    ProductCollection,
+    ProductSize,
+    ProductStatus,
+} from "../enums/product.enum";
+
+export interface Product {
+    _id: ObjectId;
+    productStatus: ProductStatus;
+    productCollection: ProductCollection;
+    productName: String;
+    productPrice: number;
+    productLeftCount: number;
+    productSize: ProductSize;
+    productVolume: number;
+    productDesc?: string;
+    productImages: string[];
+    productView: number;
+}
+
+export interface ProductInput {
+    productStatus?: ProductStatus;
+    productCollection: ProductCollection;
+    productName: String;
+    productPrice: number;
+    productLeftCount: number;
+    productSize?: ProductSize;
+    productVolume?: number;
+    productDesc?: string;
+    productImages?: string[];
+    productView: number;
+}
--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,10 +1,25 @@
+import { Product, ProductInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
+import Errors, { Message, HttpCode } from "../libs/Errors";
 
 class ProductService {
     private readonly productModel;
 
     constructor() {
         this.productModel = ProductModel;
+    }
+
+    /** SPA **/
+
+    /** SPA **/
+    public async createNewProduct(input: ProductInput): Promise<Product> {
+        try {
+           return await this.productModel.create(input); 
+        }    
+        catch (err) {
+            console.error("Error, model:createNewProduct:", err)
+        throw new Errors(HttpCode.BAD_REQUEST, Message.CEATE_FAILED);
+  }
     }
 }
 
