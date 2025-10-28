c9d0607466d44cd9a477377e0fc8020c93608d53
Doston Adikhamov <adikhamov94@gmail.com>
2025-10-29 00:28:18 +0900

    fix: modify restaurant controller processSignup and processLogin

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -14,7 +14,7 @@ routerAdmin
 routerAdmin
   .get("/signup", restaurantController.getSignup)
   .post("/signup",
-  makeUploader("members").array("memberImages", 5),
+  makeUploader("members").single("memberImage"),
   restaurantController.processSignup)
 
 routerAdmin.get("logout", restaurantController.logout);
