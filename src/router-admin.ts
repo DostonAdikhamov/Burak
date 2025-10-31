3d786e61e56e7a867f3c6cc31c5257abb7dec2cd
Doston Adikhamov <adikhamov94@gmail.com>
2025-10-31 18:37:03 +0900

    feat: develop updateChosenUser api

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -45,6 +45,8 @@ routerAdmin.post(
   productController.updateChosenProduct);
 
 /** User **/
-routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers)
+routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers);
+routerAdmin.post("/user/edit", restaurantController.verifyRestaurant, restaurantController.updateChosenUser);
+
 
 export default routerAdmin;
