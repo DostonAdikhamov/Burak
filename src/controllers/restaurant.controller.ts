71babee534526eba4919f264fa8bac9bb1cee5cf
Doston Adikhamov <adikhamov94@gmail.com>
2025-11-03 14:26:29 +0900

    54-dars: EJS Frontend signup jarayonini develop qilamiz

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -45,7 +45,6 @@ restaurantController.processSignup = async (req: AdminRequest, res: Response) =>
       throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
 
     const newMember: MemberInput = req.body;
-    console.log("req-body:", req.body);
     newMember.memberImage = file?.path.replace(/\\/g, "/");
     newMember.memberType = MemberType.RESTAURANT;
     const result = await memberService.processSignup(newMember);
--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -4,24 +4,24 @@ $(function () {
     const fileTarget = $(".file-box .upload-hidden");
     let filename;
 
-    fileTarget.on("change", function (){
+    fileTarget.on("change", function () {
         if (window.FileReader) {
             const uploadFile = $(this)[0].files[0];
             const fileType = uploadFile["type"];
             const validImageType = ["image/jpg", "image/jpeg", "image/png"];
             if (!validImageType.includes(fileType)) {
-                alert("Please insert only jpg, jpeg and png!");
+                alert("Please insert only jpg, jpeg and png!")
+            } else {
+                if(uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame")
+                    .attr("src", URL.createObjectURL(uploadFile))
+                    .addClass("Succes")
+                }
+                filename = $(this)[0].files[0].name;
             }
-        } else {
-            if (uploadFile) {
-                console.log(URL.createObjectURL(uploadFile));
-                $(".upload-img-frame")
-                .attr("src", URL.createObjectURL(uploadFile))
-                .addClass("Seccess");
-            }
-            filename = $(this)[0].file[0].name;
+            $(this).siblings(".upload-name").val(filename);
         }
-        $(this).siblings(".upload-name").val(filename);
     });
 });
 
@@ -30,7 +30,7 @@ function validateSignupForm() {
     const memberPhone = $(".member-phone").val();
     const memberPassword = $(".member-password").val();
     const confirmPassword = $(".confirm-password").val();
-    console.log (memberNick, memberPassword, memberPhone, confirmPassword);
+    
     if (
         memberNick === "" ||
         memberPhone === "" ||
@@ -46,7 +46,9 @@ function validateSignupForm() {
         return false;
     }
 
-    const memberImage = $(".member-image").get(0).files[0] ? $(".member-image").get(0).files[0].name : null;
+    const memberImage = $(".member-image").get(0).files[0] 
+    ? $(".member-image").get(0).files[0].name 
+    : null;
     if(!memberImage) {
         alert("Please, insert restaurant image!");
         return false; 
