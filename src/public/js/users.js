00936edbb16dcf14feda819929543f03295d0b69
Doston Adikhamov <adikhamov94@gmail.com>
2025-11-07 03:33:03 +0900

    feat: frontend chosen user update logic develop

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,24 @@
 console.log("Users frontend javascript file");
+
+$(function () {
+    $(".member-status").on("change", function (e) {
+        const id = e.target.id;
+        const memberStatus = $(`#${id}.member-status`).val();
+        axios.post("/admin/user/edit", {
+            _id: id,
+            memberStatus: memberStatus
+        })
+        .then((response) => {
+            const result = response.data;
+
+            if(result.data) {
+                console.log("User updated!");
+                $(".member-status").blur();
+            } else alert("User update is failed!");
+        })
+        .catch((err) => {
+            console.log(err);
+            alert("User update is failed!");
+        });
+    });
+});
